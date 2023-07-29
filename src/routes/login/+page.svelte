9311ee846a4e48f08e4e4b3d2b21c980f5d43d9c
Samandar <iwtuz166@gmail.com>
2023-07-29 23:23:26 +0500

    fix bug use barc

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-	import { onMount } from "svelte";
-	import Loader from "../../components/Loader.svelte";
-
+  import { onMount } from "svelte";
+  import Loader from "../../components/Loader.svelte";
+  
   let username = '';
   let password = '';
   let error = false;
@@ -13,7 +13,7 @@
     }
     loading = false;
   });
-
+  
   const handleSubmit = () => {
     if(!username ||!password) {
       errorName = 'Username va/yoki parolni kiriting';
@@ -33,59 +33,61 @@
     localStorage.setItem('token', username);
     window.location.href = '/';
   }
-</script>
-
-{#if loading}
-  <Loader />
-{:else}
-  <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-    <div class="max-w-md w-full space-y-8">
-      <div>
-        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Login
-        </h2>
-      </div>
-      <form class="mt-8 space-y-6" on:submit|preventDefault={handleSubmit}>
-        {#if error}
-          <div class="text-red-900 bg-red-200 py-4 px-2 rounded-md">
-            {errorName}
-          </div>
-        {/if}
-        <div class="rounded-md shadow-sm -space-y-px">
-          <div>
-            <label for="username" class="sr-only">Username</label>
-            <input
-              id="username"
-              name="username"
-              type="text"
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Username"
-              bind:value={username}
-              autocomplete="current-password"
-            />
-          </div>
-          <div>
-            <label for="password" class="sr-only">Password</label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Password"
-              bind:value={password}
-            />
-          </div>
-        </div>
-
+  </script>
+  
+  {#if loading}
+    <Loader />
+  {:else}
+    <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div class="max-w-md w-full space-y-8">
         <div>
-          <button
-            type="submit"
-            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Submit
-          </button>
+          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Login
+          </h2>
         </div>
-      </form>
+        <form class="mt-8 space-y-6" on:submit|preventDefault={handleSubmit}>
+          {#if error}
+            <div class="text-red-900 bg-red-200 py-4 px-2 rounded-md">
+              {errorName}
+            </div>
+          {/if}
+          <div class="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label for="username" class="sr-only">Username</label>
+              <input
+                id="username"
+                name="username"
+                type="text"
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Username"
+                bind:value={username}
+                autocomplete="current-password"
+              />
+            </div>
+            <div>
+              <label for="password" class="sr-only">Password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                bind:value={password}
+                autocomplete="current-password"
+              />
+            </div>
+          </div>
+  
+          <div>
+            <button
+              type="submit"
+              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-{/if}
+  {/if}
+  
