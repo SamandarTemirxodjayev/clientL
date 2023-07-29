d576dc2ca9a85acda474db027062d9c737372e2b
Samandar <iwtuz166@gmail.com>
2023-07-29 23:20:25 +0500

    fix bugs

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -61,6 +61,7 @@
               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Username"
               bind:value={username}
+              autocomplete="current-password"
             />
           </div>
           <div>
