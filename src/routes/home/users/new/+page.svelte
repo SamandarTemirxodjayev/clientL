2e8fd9427641ce2d38fd27f1daead656c1fc97ec
Samandar <iwtuz166@gmail.com>
2023-08-01 20:59:09 +0500

    fix

--- src/routes/home/users/new/+page.svelte
+++ src/routes/home/users/new/+page.svelte
@@ -20,7 +20,7 @@
       name: name,
       surname: surname,
       id_card: id_card,
-      phone_number: phone_number,
+      phone_number: "998" + phone_number,
     });
     window.location.href ="/home/users";
     } catch (error) {
@@ -49,8 +49,11 @@
       <input bind:value={id_card} type="text" id="id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 outline-blue-500" placeholder="AB1231212" required>
     </div>
     <div class="mb-4">
-      <label for="phone" class="block mb-2 text-sm font-medium text-gray-90">Telefon Raqami (+ belgisiz)</label>
-      <input bind:value={phone_number} type="text" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 outline-blue-500" placeholder="998901234567" required>
+      <label for="phone" class="block mb-2 text-sm font-medium text-gray-90">Telefon Raqami</label>
+      <div class="flex items-center">
+        <div class="bg-gray-200 p-2.5 text-sm border-t border-l border-b border-gray-300">+998</div>
+        <input bind:value={phone_number} type="text" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-r-lg block w-full p-2.5 outline-blue-500" placeholder="901234567" required>
+      </div>
     </div>
     <input type="submit" value="Submit" class="text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm min-w-full sm:w-auto px-5 py-2.5 text-center" />
   </form>
